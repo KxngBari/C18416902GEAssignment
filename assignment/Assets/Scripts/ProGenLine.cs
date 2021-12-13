bb5d86e853902254a2732fa1c6ff47d245fc9324
KxngBari <49103177+KxngBari@users.noreply.github.com>
2021-12-13 01:13:31 +0000

    calculating the angle of the circle

--- assignment/Assets/Scripts/ProGenLine.cs
+++ assignment/Assets/Scripts/ProGenLine.cs
@@ -26,7 +26,30 @@ public class ProGenLine : MonoBehaviour
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        //DrawCircle();
+        //call on the draw circle function here
+
+    }
+
+    void DrawCircle()
+    {
+        lineRenderer.positionCount = numberOfPoints + 1;
+
+        for(int i = 0; i < numberOfPoints; i++)
+        {
+            // i have to calculate the angle of the circle
+            float angle = 2f * Mathf.PI / (float)numberOfPoints * i;
+
+            float lineXPosition = Mathf.Cos(angle) * radius;
+            float lineYPosition = Mathf.Sin(angle) * radius;
+
+            //calculate its offset
+            //apply a distance that is random
+
+            //set the position on the line renderer
+
+        }
+
+        //close the circle by calculating the last point's position
     }
 
     // Update is called once per frame
