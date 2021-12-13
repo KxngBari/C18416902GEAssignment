9efef042048cc49310b45be8648e4a93fcb3cb46
KxngBari <49103177+KxngBari@users.noreply.github.com>
2021-12-13 00:59:29 +0000

    Implementing possible settings the user can change

--- assignment/Assets/Scripts/ProGenLine.cs
+++ assignment/Assets/Scripts/ProGenLine.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
+
 public class ProGenLine : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    [Range(0.0f,10.0f)]
+    private float radius = 1.0f;
+
+    [SerializeField]
+    private Vector2 randomOffset = Vector2.zero;
+
+    [SerializeField]
+    [Range(1,1000)]
+    private int numberOfPoints = 100;
+
+    [SerializeField]
+    [Range(0.0f, 10.0f)]
+    private float updateSeconds = 0.5f;
+
+    private LineRenderer lineRenderer;
+
     void Start()
     {
-        
+        lineRenderer = GetComponent<LineRenderer>();
+        //DrawCircle();
     }
 
     // Update is called once per frame
